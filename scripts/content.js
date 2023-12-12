9fc50111e2eda9917c4f7e9a430313e2339dbe7d
subsurfmodifier <a2plewins@gmail.com>
2023-12-12 13:27:21 +0900

    fixing issues with the revert

--- scripts/content.js
+++ scripts/content.js
@@ -1,18 +1,50 @@
+let itemNameQuery = null;
+let priceQuery = null;
+let quantityQuery = null;
+let elementString = null;
+const elementList = [];
+let item = null;
+let price = null;
+let quantity = null;
+
 const observer = new MutationObserver(function (mutations, mutationInstance) {
   mutations.forEach(function (mutation) {
-       if (mutation.type === 'childList' && mutation.addedNodes.length > 0) {
+    if (mutation.type === 'childList' && mutation.addedNodes.length > 0) {
       mutation.addedNodes.forEach(function (addedNode) {
         if (addedNode.id === 'cart_body' && addedNode.firstChild.className === 'inner_cont') {
-          const query = 'body > div:nth-child(2) > div:nth-child(2) > div > div > div:nth-child(2) > div > div:nth-child(1) > ol > li:nth-child(n) > div.cart--basket_body > div > ul > li > div > div.item_info > dl > dd > div.section.item_title > a > span';
-          const itemList = document.querySelectorAll(query);
-          let elementList = [];
-          itemList.forEach((item) => {
-            elementList.push(item.textContent); 
-          });
-          chrome.runtime.sendMessage({type: 'variable', data: elementList});         
-        } 
+          const count = document.querySelectorAll('#cart_list > ol > li').length;
+          for (let i = 1; i <= count; i++) {
+            itemNameQuery = '#cart_list > ol > li:nth-child(' + i + ') > div.cart--basket_body > div > ul > li > div > div.item_info > dl > dd > div.section.item_title > a > span';
+            priceQuery = '#cart_list > ol > li:nth-child(' + i + ') > div.cart--basket_body > div > ul > li > div > div.item_info > dl > dd > div.section__wrapper > div.section__item--right > div.section.item_price > span > span > strong';
+            quantityQuery = '#cart_list > ol > li:nth-child(' + i + ') > div.cart--basket_body > div > ul > li > div > div.item_info > dl > dd > div.section__wrapper > div.section__item--left > div.section.item_qty > div > input';
+            item = document.querySelector(itemNameQuery);
+            if (item !== null) {
+              price = document.querySelector(priceQuery);
+              if (price !== null) {
+                let elementString = item.textContent + ',' + price.textContent.replace(/,/g, '');
+                quantity = document.querySelector(quantityQuery);
+                if (quantity !== null) {
+                  console.log('lulu');
+                  elementString += ',' + quantity.value;
+                  console.log(elementString);
+                  elementList.push(elementString);
+
+                } else {
+                  console.log(i + 'th quantity is not found');
+                }
+              } else {
+                console.log(i + 'th price is not found');
+              }
+            } else {
+              console.log(i + 'th item is not found');
+            }
+          };
+          console.log('beautiful');
+          console.log(elementList);
+          chrome.runtime.sendMessage({ type: 'variable', data: elementList });
+        }
       });
-    } 
+    }
   });
 });
 
@@ -20,8 +52,3 @@ observer.observe(document, {
   childList: true,
   subtree: true,
 });
-
-
-
-
-
