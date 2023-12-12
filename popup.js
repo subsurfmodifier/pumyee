e6f3b83badcaeda7ddc5f3482040856a6ca13cd9
subsurfmodifier <a2plewins@gmail.com>
2023-12-12 12:13:55 +0900

    add price and quantity column and prevented downloading multiple files

--- popup.js
+++ popup.js
@@ -1,20 +1,26 @@
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
     if (message.type === 'variable') {
         const elementList = message.data;
-        const itemName = 'Item' + '\n' + elementList.join('\n');
+        const itemName = 'Item, Price, Quantity' + '\n' + elementList.join('\n');
         const blob = new Blob([new Uint8Array([0xEF, 0xBB, 0xBF]), itemName], {type:'text/csv;charset=utf-8'});
         console.log(blob);
         console.log('lala');
         const button = document.getElementById('download');
         console.log(button);
-        button.addEventListener('click', function () {
+
+        button.removeEventListener('click', handleButtonClick);
+
+        button.addEventListener('click', handleButtonClick);
+
+        function handleButtonClick() {
             const link = document.createElement('a');
             link.href = URL.createObjectURL(blob);
             link.download = 'data.csv';
             link.click();
-        });
-    };
+        }
+    }
 });
+
 
 
 
