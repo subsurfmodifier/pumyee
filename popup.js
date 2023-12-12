fb974355a7cebe5b8a3048f670f6f6f4502080f9
subsurfmodifier <122791338+subsurfmodifier@users.noreply.github.com>
2023-12-12 12:58:55 +0900

    Revert "Add price column"

--- popup.js
+++ popup.js
@@ -1,31 +1,4 @@
-chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-    if (message.type === 'variable') {
-        const elementList = message.data;
-        const itemName = 'Item, Price, Quantity' + '\n' + elementList.join('\n');
-        const blob = new Blob([new Uint8Array([0xEF, 0xBB, 0xBF]), itemName], {type:'text/csv;charset=utf-8'});
-        console.log(blob);
-        console.log('lala');
-        const button = document.getElementById('download');
-        console.log(button);
-
-        button.removeEventListener('click', handleButtonClick);
-
-        button.addEventListener('click', handleButtonClick);
-
-        function handleButtonClick() {
-            const link = document.createElement('a');
-            link.href = URL.createObjectURL(blob);
-            link.download = 'data.csv';
-            link.click();
-        }
-    }
-});
-
-
-
-
-
-
-
-
+document.getElementById('myButton').addEventListener('click', function() {
+    alert('Button clicked!');
+  });
   
