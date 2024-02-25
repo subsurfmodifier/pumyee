002df218ec8af9bc7ebdae22b6ca9e579f8cb120
subsurfmodifier <subsurfmodifier@github.com>
2024-02-25 13:45:04 +0900

    message conflict

--- popup.js
+++ popup.js
@@ -1,22 +1,37 @@
-chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-    if (message.type === 'variable') {
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+  if(message == 'content up and running') {
+    console.log('got the message from the popup side');
+      const create = document.getElementById('create');
+    console.log(create);
+    function createCSV() {
+      chrome.runtime.sendMessage('createCSV');
+    };
+    create.addEventListener('click', createCSV);
+    create.addEventListener('click', console.log('clicking'));
+    
+    chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+      if (message.type === 'variable') {
         const elementList = message.data;
         const itemName = 'Item, Price, Quantity' + '\n' + elementList.join('\n');
         const blob = new Blob([new Uint8Array([0xEF, 0xBB, 0xBF]), itemName], {type:'text/csv;charset=utf-8'});
         console.log(blob);
         console.log('lala');
-        const button = document.getElementById('download');
+        const download = document.createElement('button');
+        download.id = 'download';
+        download.addEventListener('click', downloadCSV);
+        function downloadCSV() {
+          const link = document.createElement('a');
+          link.href = URL.createObjectURL(blob);
+          link.download = 'data.csv';
+          link.click();
+        };
+      }
+    });
+    }});
+    
+ 
 
-        button.addEventListener('click', handleButtonClick);
 
-        function handleButtonClick() {
-            const link = document.createElement('a');
-            link.href = URL.createObjectURL(blob);
-            link.download = 'data.csv';
-            link.click();
-        }
-    }
-});
 
 
 
--- service_worker.js
+++ service_worker.js
@@ -1,3 +1,21 @@
+console.log('background running');
+
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+    if(message === 'content up and running') {
+        console.log('content up and running');
+        chrome.runtime.sendMessage('content up and running');
+    }
+})
+
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+    if(message === 'createCSV') {
+        console.log('received');
+        chrome.runtime.sendMessage('createCSV');
+    }
+})
+
+console.log('done');
+
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
     if (message.type === 'variable') {
         chrome.runtime.sendMessage(message);
