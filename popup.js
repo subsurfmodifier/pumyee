e9e7175374e997d9b098802c50cb6bb4362eed69
subsurfmodifier <subsurfmodifier@github.com>
2024-02-25 20:22:43 +0900

    fixed message

--- popup.js
+++ popup.js
@@ -5,10 +5,5 @@ console.log(create);
 create.addEventListener('click', sendMessage);
 async function sendMessage() {
   const activeTab = await chrome.tabs.query({active: true, currentWindow: true});
-  const response = await chrome.tabs.sendMessage(activeTab[0].id, {message: 'lala'});
-  console.log(response);
-
-  chrome.tabs.sendMessage(activeTab[0].id, {message: 'lala'}, function (response) {
-    console.log(response);
-  });
+  chrome.tabs.sendMessage(activeTab[0].id, {message: 'lala'});
 }
--- scripts/content.js
+++ scripts/content.js
@@ -3,14 +3,12 @@ console.log('content.js running');
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     if(request.message === 'lala') {
             console.log('lala received');
-            // const link = document.createElement('a');
+            const link = document.createElement('a');
             const blob = new Blob(['lala']);
-            sendResponse(blob);
-            // link.href = URL.createObjectURL(blob);
-            // link.download = 'data.csv';
-            // link.click();
+            link.href = URL.createObjectURL(blob);
+            link.download = 'data.csv';
+            link.click();
     }
-    return true;
 })
 
 
