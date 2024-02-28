8507bab08ac61c2b89cacbb1a6f374bd3b68bf3c
subsurfmodifier <subsurfmodifier@github.com>
2024-02-28 12:11:40 +0900

    displaying download only on the correct website

--- popup.js
+++ popup.js
@@ -1,17 +1,25 @@
 console.log('popup running');
+
+const download = document.getElementById('download');
+const warningDiv = document.getElementById('warning');
+const referenceDiv = document.getElementById('reference');
+const wrongWebsiteDiv = document.getElementById('wrongWebsite');
+warningDiv.style.display = 'none';
+referenceDiv.style.display = 'none';
+wrongWebsiteDiv.style.display = 'block';
+download.style.display = 'none';
 async function addReference() {
   const currentTab =  await chrome.tabs.query({active: true, currentWindow: true});
   if(currentTab[0].url === "https://www.i-screammall.co.kr/order/cart") {
     referenceDiv.style.display = 'block';
+  } 
+  if(currentTab[0].url === "https://www.i-screammall.co.kr/order/cart"|currentTab[0].url == "https://cart.gmarket.co.kr/ko/pc/cart/#/") {
+    wrongWebsiteDiv.style.display = 'none';
+    download.style.display = 'block';
   }
 }
 addReference();
-const warningDiv = document.getElementById('warning');
-const referenceDiv = document.getElementById('reference');
-warningDiv.style.display = 'none';
-referenceDiv.style.display = 'none';
 
-const download = document.getElementById('download');
 download.addEventListener('click', sendMessage);
 async function sendMessage() {
   const activeTab = await chrome.tabs.query({active: true, currentWindow: true});
